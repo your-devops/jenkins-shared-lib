9414df8648f7085802d28b80345f30540efb7403
Fiodor Andruhin <fandruhin@ms3-inc.com>
2020-09-14 15:34:35 +0300

    changed way docker agent is used

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,31 +1,41 @@
 def call(Map pipelineParams) {
 
   pipeline {
-    agent none
+    agent any
     stages {
       stage('Prepare Env Vars') {
-        agent {
-          docker {
-            image 'jakubsacha/docker-xmlstarlet:latest'
-            args "--entrypoint=''"
-          }
-        }
         steps {
           script {
-            // Set default requiredApplicationCoverage for MUnit tests. May be overriden from Job Properties.
-            if (env.requiredApplicationCoverage == null) {
-              env.requiredApplicationCoverage = "75"
-            // Set tests to fail build if Application Coverage is below required value
-            helper.pomSetMunitConfig('failBuild','true')
-            // Set requiredApplicationCoverage
-            helper.pomSetMunitConfig('requiredApplicationCoverage',requiredApplicationCoverage)
-            // test
-            sh "cat pom.xml"
+            docker.image('jakubsacha/docker-xmlstarlet:latest').inside {
+              // Set default requiredApplicationCoverage for MUnit tests. May be overriden from Job Properties.
+              if (env.requiredApplicationCoverage == null) {
+                env.requiredApplicationCoverage = "75"
+              // Set tests to fail build if Application Coverage is below required value
+              helper.pomSetMunitConfig('failBuild','true')
+              // Set requiredApplicationCoverage
+              helper.pomSetMunitConfig('requiredApplicationCoverage',requiredApplicationCoverage)
+              // test
+              sh "cat pom.xml"
             }
           }
         }
       }
       stage('Unit Tests') {
+        steps {
+          script {
+            docker.image('maven:3.6-jdk-8').inside("-v $HOME/.m2:/root/.m2 -u root") {
+              sh "cat pom.xml"
+              // Run Unit Tests
+              configFileProvider([configFile(fileId: 'maven_settings', variable: 'mavenSettingsFile')]) {
+                withCredentials([usernamePassword(credentialsId: 'dev-encryptor-pwd', passwordVariable: 'encryptorPasswd', usernameVariable: 'anypointUser')]) {
+                  sh "mvn -s '$mavenSettingsFile' clean test -Denv=${environment} -Dapp.key=${encryptorPasswd}"
+                }
+              }
+            }
+          }
+        }
+      }
+      stage('Unit Tests 2') {
         agent {
           docker {
             image 'maven:3.6-jdk-8'
