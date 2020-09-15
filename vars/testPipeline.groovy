20f579c351956c75e5b968660672a1a0ec390698
Fiodor Andruhin <fandruhin@ms3-inc.com>
2020-09-15 20:39:10 +0300

    test stashing

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -1,11 +1,12 @@
 def call(Map pipelineParams) {
 
   pipeline {
-    agent {
-      label 'master'
-    }
+    agent none
     stages {
       stage('Prepare Env Vars') {
+        agent {
+          label 'master'
+        }
         steps {
           script {
             LAST_EXECUTED_STAGE = STAGE_NAME
@@ -13,7 +14,7 @@ def call(Map pipelineParams) {
             if (env.requiredApplicationCoverage == null) {
               env.requiredApplicationCoverage = "75"
             }
-            docker.image('jakubsacha/docker-xmlstarlet:latest').inside("--entrypoint=''") {
+            docker.image('yourdevops/docker-xmlstarlet:latest').inside("--entrypoint=''") {
               // Set tests to fail build if Application Coverage is below required value
               helper.pomSetMunitConfig('failBuild','true')
               // Set requiredApplicationCoverage
@@ -21,39 +22,40 @@ def call(Map pipelineParams) {
               // Output changed pom.xml
               echo "Building with following pom.xml:"
               sh "cat pom.xml"
+              stash includes: 'pom.xml', name: 'parsedPom'
             }
           }
         }
       }
       stage('Unit Tests') {
+        agent {
+          docker {
+            image 'maven:3.6-jdk-8'
+            args '-v $HOME/.m2:/root/.m2 -u root'
+          }
+        }
         steps {
           script {
             LAST_EXECUTED_STAGE = STAGE_NAME
-            docker.image('maven:3.6-jdk-8').inside("-v $HOME/.m2:/root/.m2 -u root") {
-              // Run Unit Tests
-              configFileProvider([configFile(fileId: 'maven_settings', variable: 'mavenSettingsFile')]) {
-                withCredentials([usernamePassword(credentialsId: 'dev-encryptor-pwd', passwordVariable: 'encryptorPasswd', usernameVariable: 'anypointUser')]) {
-                  sh "mvn -s '$mavenSettingsFile' clean test -Denv=${environment} -Dapp.key=${encryptorPasswd}"
-                }
+            unstash 'parsedPom'
+            sh "cat pom.xml"
+            // Run Unit Tests
+            configFileProvider([configFile(fileId: 'maven_settings', variable: 'mavenSettingsFile')]) {
+              withCredentials([usernamePassword(credentialsId: 'dev-encryptor-pwd', passwordVariable: 'encryptorPasswd', usernameVariable: 'anypointUser')]) {
+                sh "mvn -s '$mavenSettingsFile' clean test -Denv=${environment} -Dapp.key=${encryptorPasswd}"
               }
             }
- 
-          publishHTML (target: [
-            allowMissing: true,
-            alwaysLinkToLastBuild: false,
-            keepAll: true,
-            reportDir: '.',
-            reportFiles: 'linter-output.txt',
-            reportName: "Linter Output"
-          ])
           }
         }
       }
     }
     post {
       always {
-        script {
-          echo "*${currentBuild.currentResult}:* Job <${env.JOB_URL}|${env.JOB_NAME}> <${env.BUILD_URL}console|build #${BUILD_NUMBER}>:\\n${currentBuild.getBuildCauses()[0].shortDescription}\\nTime total: ${currentBuild.durationString}"
+        node("master") {
+          script {
+            SLACK_REPORT_MSG = "*${currentBuild.currentResult}:* Job <${env.JOB_URL}|${env.JOB_NAME}> <${env.BUILD_URL}console|build #${BUILD_NUMBER}>:\n${currentBuild.getBuildCauses()[0].shortDescription}\nFailed on stage:${LAST_EXECUTED_STAGE}\nTime total: " + sh(script: "echo '${currentBuild.durationString}' | sed 's/and counting//'", returnStdout: true).trim()
+            echo SLACK_REPORT_MSG
+          }
         }
       }
     }
