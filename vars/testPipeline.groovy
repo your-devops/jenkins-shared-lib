de6e3a4c456236b1e7fb78b509dd93d420b883db
Fiodor Andruhin <fandruhin@ms3-inc.com>
2020-09-22 15:54:24 +0300

    another test -fix branch #2

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -18,7 +18,7 @@ def call(Map pipelineParams) {
     post {
       always {
         node("master") {
-          jiraSendBuildInfo site: 'yourdevops.atlassian.net', branch: "${GIT_BRANCH}"
+          jiraSendBuildInfo site: 'yourdevops.atlassian.net', branch: GIT_BRANCH
           script {
             if (env.JIRA_ISSUE_KEY != null) echo "JIRA_ISSUE_KEY: ${env.JIRA_ISSUE_KEY}"
             SLACK_REPORT_MSG = "*${currentBuild.currentResult}:* Job <${env.BUILD_URL}display/redirect|${env.JOB_NAME} build #${BUILD_NUMBER}>:\n${currentBuild.getBuildCauses()[0].shortDescription}\nFailed on stage: ${LAST_EXECUTED_STAGE}\nTime total: " + sh(script: "echo '${currentBuild.durationString}' | sed 's/and counting//'", returnStdout: true).trim()
