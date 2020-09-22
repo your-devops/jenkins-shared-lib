0404f27afe60f43c400f051f239dba944f905b6d
Fiodor Andruhin <fandruhin@ms3-inc.com>
2020-09-22 16:06:36 +0300

    another test -fix branch #7

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -21,7 +21,7 @@ def call(Map pipelineParams) {
         node("master") {
           env.BITBUCKET_SOURCE_BRANCH = sh(script: "echo $GIT_BRANCH | grep -oP '(?<=origin/).*'", returnStdout: true).trim()
           jiraSendBuildInfo site: 'yourdevops.atlassian.net', branch: "${env.BITBUCKET_SOURCE_BRANCH}"
-          jiraSendDeploymentInfo site: 'example.atlassian.net', branch: "${env.BITBUCKET_SOURCE_BRANCH}", environmentId: 'qa-1', environmentName: 'QA-1', environmentType: 'QA'
+          jiraSendDeploymentInfo site: 'example.atlassian.net', environmentId: 'qa-1', environmentName: 'QA-1', environmentType: 'QA'
           script {
             if (env.JIRA_ISSUE_KEY != null) echo "JIRA_ISSUE_KEY: ${env.JIRA_ISSUE_KEY}"
             SLACK_REPORT_MSG = "*${currentBuild.currentResult}:* Job <${env.BUILD_URL}display/redirect|${env.JOB_NAME} build #${BUILD_NUMBER}>:\n${currentBuild.getBuildCauses()[0].shortDescription}\nFailed on stage: ${LAST_EXECUTED_STAGE}\nTime total: " + sh(script: "echo '${currentBuild.durationString}' | sed 's/and counting//'", returnStdout: true).trim()
