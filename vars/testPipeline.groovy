ff5deb8726f89043812799f2e193b9c2f8feab17
Fiodor Andruhin <fandruhin@ms3-inc.com>
2020-09-22 14:08:30 +0300

    another test

--- vars/testPipeline.groovy
+++ vars/testPipeline.groovy
@@ -10,40 +10,7 @@ def call(Map pipelineParams) {
         steps {
           script {
             LAST_EXECUTED_STAGE = STAGE_NAME
-            // Set default requiredApplicationCoverage for MUnit tests. May be overriden from Job Properties.
-            if (env.requiredApplicationCoverage == null) {
-              env.requiredApplicationCoverage = "75"
-            }
-            docker.image('yourdevops/docker-xmlstarlet:latest').inside("--entrypoint=''") {
-              // Set tests to fail build if Application Coverage is below required value
-              helper.pomSetMunitConfig('failBuild','true')
-              // Set requiredApplicationCoverage
-              helper.pomSetMunitConfig('requiredApplicationCoverage',requiredApplicationCoverage)
-              // Output changed pom.xml
-              echo "Building with following pom.xml:"
-              sh "cat pom.xml"
-              stash includes: 'pom.xml', name: 'parsedPom'
-            }
-          }
-        }
-      }
-      stage('Unit Tests') {
-        agent {
-          docker {
-            image 'maven:3.6-jdk-8'
-            args '-v $HOME/.m2:/root/.m2 -u root'
-          }
-        }
-        steps {
-          script {
-            LAST_EXECUTED_STAGE = STAGE_NAME
-            unstash 'parsedPom'
-            sh "cat pom.xml"
-            // Run Unit Tests
-            configFileProvider([configFile(fileId: 'maven_settings', variable: 'mavenSettingsFile')]) {
-              withCredentials([usernamePassword(credentialsId: 'dev-encryptor-pwd', passwordVariable: 'encryptorPasswd', usernameVariable: 'anypointUser')]) {
-                sh "mvn -s '$mavenSettingsFile' clean test -Denv=${environment} -Dapp.key=${encryptorPasswd}"
-              }
+            sh "git status"
             }
           }
         }
